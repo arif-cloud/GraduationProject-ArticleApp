0b64a9603c261216f0505d3ddbae1156302aae75
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2024-03-21 13:59:10 +0300

    Constants defined

---
+++ app/src/main/java/com/example/articleapp/common/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.articleapp.common
+
+object Constants {
+    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+}
+
+object Graph {
+    const val ROOT = "root_graph"
+    const val AUTHENTICATION = "auth_graph"
+    const val HOME = "home_graph"
+}
