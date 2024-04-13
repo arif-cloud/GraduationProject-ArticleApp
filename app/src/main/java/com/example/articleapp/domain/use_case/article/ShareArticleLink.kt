9a3b04d1429251bbb71ffe17b514ee07f295cf39
Arif Demiryas <73065590+arif-cloud@users.noreply.github.com>
2024-04-13 23:49:32 +0300

    Share article link use case updated

--- app/src/main/java/com/example/articleapp/domain/use_case/article/ShareArticleLink.kt
+++ app/src/main/java/com/example/articleapp/domain/use_case/article/ShareArticleLink.kt
@@ -2,18 +2,17 @@ package com.example.articleapp.domain.use_case.article
 
 import android.content.Context
 import android.content.Intent
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class ShareArticleLink @Inject constructor(
-    @ApplicationContext private val context : Context
+    private val context : Context
 ) {
     operator fun invoke(url : String) {
         val sendIntent = Intent(Intent.ACTION_SEND).apply {
             putExtra(Intent.EXTRA_TEXT, url)
             type = "text/plain"
         }
-        val shareIntent = Intent.createChooser(sendIntent, null)
+        val shareIntent = Intent.createChooser(sendIntent, null).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         context.startActivity(shareIntent)
     }
 }
